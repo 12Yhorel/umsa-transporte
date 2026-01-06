fd08545db59c18bce0c83937f8d55d4cd4c5b6f3
Yhared Alvarez <yalvareza@fcpn.edu.bo>
2026-01-06 16:11:13 -0400

    fix: deshabilitar webSecurity para desarrollo y evitar errores CSP

--- electron/main.js
+++ electron/main.js
@@ -76,7 +76,8 @@ function createWindow() {
       webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
         contextIsolation: true,
-        nodeIntegration: false
+        nodeIntegration: false,
+        webSecurity: false
       },
       show: false
     });
